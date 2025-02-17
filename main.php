52bc029736fc7019cd2e25d2e56a9b9bf6a3eb3c
chokopay <vazovmikola@gmail.com>
2025-02-17 21:26:56 +0100

    2
    updated

--- main.php
+++ main.php
@@ -3,43 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>learn php</title>
-    <link href="t1.css" media="all" rel="Stylesheet" type="text/css" /> 
+    <title>music player</title>
+    <link href="style.css" media="all" rel="Stylesheet" type="text/css" /> 
 </head>
 <body>
-    <div id="header">
+<div id="header">
         <ul>
-            <li><a href="">home</a></li>
-            <li><a href="https://github.com/vyazovmykola">github</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="https://github.com/vyazovmykola">GitHub</a></li>
         </ul>
     </div>
+
     <div id="main">
         <div id="sideleft">
-        <ul>
-            <li>Tyler,The creator</li>
-            <li>Frank Ocean</li>
-            <li>Odd Future</li>
-            <li>Alex G</li>
-            <li>5</li>
-        </ul>
+            <ul>
+                <li><a href="#" class="artist-link" data-artist="tyler">Tyler, The Creator</a></li>
+                <li><a href="#" class="artist-link" data-artist="frank">Frank Ocean</a></li>
+                <li><a href="#" class="artist-link" data-artist="odd_future">Odd Future</a></li>
+                <li><a href="#" class="artist-link" data-artist="alex_g">Alex G</a></li>
+            </ul>
         </div>
 
         <div id="tracks">
-            <button class="track_list" onclick="play_track('Frank Ocean – Pyramids.mp3')">
-                <img class="image" src="channel-orange.jpg">
-                Pyramids<br>Frank Ocean
-            </button>
-            
-            <button class="track_list" onclick="play_track('Frank Ocean – white ferrari.mp3')">
-                <img class="image" src="blonde.jpg">
-                White Ferrari <br> Frank Ocean
-            </button>
-
+            <p id="welcome_text">choose artist to load trecks</p>
+            <div id="track-list"></div>
             <audio id="audio_player" controls>
-                your browser dont work with this audio
+                Your browser does not support audio playback.
             </audio>
         </div>
     </div>
+
     <script src="script.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,5 +1,50 @@
-function play_track(src){
+const tracks = {
+    "tyler": [
+        { src: "music/01. St. Chroma.mp3", img: "chromakopia.jpg", name: "St. Chroma", artist: "Tyler, The Creator" },
+        { src: "music/02. Rah Tah Tah.mp3", img: "chromakopia.jpg", name: "Rah Tah Tah", artist: "Tyler, The Creator" },
+        { src: "music/03. Noid.mp3", img: "chromakopia.jpg", name: "Noid", artist: "Tyler, The Creator" },
+        { src: "music/04. Darling, I.mp3", img: "chromakopia.jpg", name: "Darling, I", artist: "Tyler, The Creator" }       
+    ],
+    "frank": [
+        { src: "music/Frank Ocean – Pyramids.mp3", img: "channel-orange.jpg", name: "Pyramids", artist: "Frank Ocean" },
+        { src: "music/Frank Ocean – white ferrari.mp3", img: "blonde.jpg", name: "White Ferrari", artist: "Frank Ocean" }
+    ]
+};
+
+function loadTracks(artist) {
+    let trackListContainer = document.getElementById("track-list");
+    trackListContainer.innerHTML = ""; // clean trecks
+    let welcome_text_clear = document.getElementById("welcome_text");
+    welcome_text_clear.innerHTML = "enjoy your songs";
+
+    tracks[artist].forEach(track => {
+        let button = document.createElement("button");
+        button.classList.add("track_list");
+        button.innerHTML = `
+            <img class="image" src="${track.img}">
+            ${track.name} <br> ${track.artist}
+        `;
+        button.onclick = () => playTrack(track.src);
+        trackListContainer.appendChild(button);
+    });
+}
+
+function playTrack(src) {
     let player = document.getElementById("audio_player");
+    if (!player) {
+        console.error("error");
+        return;
+    }
     player.src = src;
     player.play();
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll(".artist-link").forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const artist = this.getAttribute("data-artist");
+            loadTracks(artist);
+        });
+    });
+});
